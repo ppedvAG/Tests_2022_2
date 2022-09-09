a549cd63d788fc7d746c22d04c40327662337a8f
André <AndreR@ppedv.de>
2022-09-09 15:33:33 +0200

    test hübscher

--- Stammdatenverwaltung/Stammdatenverwaltung.Logic.Tests/MitarbeiterManagerTests.cs
+++ Stammdatenverwaltung/Stammdatenverwaltung.Logic.Tests/MitarbeiterManagerTests.cs
@@ -2,13 +2,11 @@ using Moq;
 using Stammdatenverwaltung.Model;
 using Stammdatenverwaltung.Model.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Stammdatenverwaltung.Logic.Tests
 {
-    public class MitarbeiterManagerTests
+    public partial class MitarbeiterManagerTests
     {
         [Theory]
         [InlineData(1966, 9, 9, 56)]//Adam Sandler
@@ -29,7 +27,15 @@ namespace Stammdatenverwaltung.Logic.Tests
 
         [Fact]
         public void GetAbteilungWithYoungestAverageMitarbeiter_Empty_DB_returns_null()
-        { }
+        {
+            var testDay = new DateTime(2022, 9, 9);
+            var repoMock = new Mock<IRepository>();
+            var mm = new MitarbeiterManager(repoMock.Object);
+
+            var result = mm.GetAbteilungWithYoungestAverageMitarbeiter(testDay);
+
+            Assert.Null(result);
+        }
 
         [Fact]
         public void GetAbteilungWithYoungestAverageMitarbeiter_3_Abts_Number_2_wins_moq()
@@ -67,54 +73,6 @@ namespace Stammdatenverwaltung.Logic.Tests
             var result = mm.GetAbteilungWithYoungestAverageMitarbeiter(testDay);
 
             Assert.Equal("Abt2", result.Bezeichnung);
-        }
-
-        class TestRepo : IRepository
-        {
-            public void Add<T>(T entity) where T : Entity
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Delete<T>(T entity) where T : Entity
-            {
-                throw new NotImplementedException();
-            }
-
-            public IEnumerable<T> GetAll<T>() where T : Entity
-            {
-                if (typeof(T) == typeof(Abteilung))
-                {
-                    var abt1 = new Abteilung() { Bezeichnung = "Abt1" };
-                    abt1.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-100) });
-                    abt1.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-200) });
-
-                    var abt2 = new Abteilung() { Bezeichnung = "Abt2" };
-                    abt2.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-20) });
-                    abt2.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-30) });
-
-                    var abt3 = new Abteilung() { Bezeichnung = "Abt3" };
-                    abt3.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-400) });
-
-                    return new[] { abt1, abt2, abt3 }.Cast<T>();
-                }
-                throw new NotImplementedException();
-            }
-
-            public T GetById<T>(int id) where T : Entity
-            {
-                throw new NotImplementedException();
-            }
-
-            public int SaveAll()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Update<T>(T entity) where T : Entity
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 }
---
+++ Stammdatenverwaltung/Stammdatenverwaltung.Logic.Tests/TestRepo.cs
@@ -0,0 +1,59 @@
+﻿using Stammdatenverwaltung.Model;
+using Stammdatenverwaltung.Model.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stammdatenverwaltung.Logic.Tests
+{
+    public partial class MitarbeiterManagerTests
+    {
+        class TestRepo : IRepository
+        {
+            public void Add<T>(T entity) where T : Entity
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete<T>(T entity) where T : Entity
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<T> GetAll<T>() where T : Entity
+            {
+                if (typeof(T) == typeof(Abteilung))
+                {
+                    var abt1 = new Abteilung() { Bezeichnung = "Abt1" };
+                    abt1.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-100) });
+                    abt1.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-200) });
+
+                    var abt2 = new Abteilung() { Bezeichnung = "Abt2" };
+                    abt2.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-20) });
+                    abt2.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-30) });
+
+                    var abt3 = new Abteilung() { Bezeichnung = "Abt3" };
+                    abt3.Mitarbeiter.Add(new Mitarbeiter() { GebDatum = DateTime.Now.AddYears(-400) });
+
+                    return new[] { abt1, abt2, abt3 }.Cast<T>();
+                }
+                throw new NotImplementedException();
+            }
+
+            public T GetById<T>(int id) where T : Entity
+            {
+                throw new NotImplementedException();
+            }
+
+            public int SaveAll()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update<T>(T entity) where T : Entity
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
