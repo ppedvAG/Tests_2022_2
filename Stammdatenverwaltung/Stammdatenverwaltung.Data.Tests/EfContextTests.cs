63c4b1e8c32857b443abb840bb3eabdfa61cc7d3
André <AndreR@ppedv.de>
2022-09-09 11:15:28 +0200

    CRUD Tests für Mitarbeiter

--- Stammdatenverwaltung/Stammdatenverwaltung.Data.Tests/EfContextTests.cs
+++ Stammdatenverwaltung/Stammdatenverwaltung.Data.Tests/EfContextTests.cs
@@ -1,5 +1,6 @@
+using Stammdatenverwaltung.Data.Model;
 using System;
-using System.Threading;
+using System.Linq;
 using Xunit;
 
 namespace Stammdatenverwaltung.Data.Tests
@@ -17,5 +18,75 @@ namespace Stammdatenverwaltung.Data.Tests
 
             Assert.True(wasCreate);
         }
+
+        [Fact]
+        public void Can_add_new_Mitarbeitert_to_db()
+        {
+            var m = new Mitarbeiter { Name = $"Fred_{Guid.NewGuid()}" };
+
+            using (var con = new EfContext())
+            {
+                con.Mitarbeiter.Add(m);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.Equal(m.Name, loaded.Name);
+            }
+        }
+
+        [Fact]
+        public void Can_update_Mitarbeitert()
+        {
+            var m = new Mitarbeiter { Name = $"Fred_{Guid.NewGuid()}" };
+            var newName = $"Wilma_{Guid.NewGuid()}";
+            using (var con = new EfContext())
+            {
+                con.Mitarbeiter.Add(m);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                loaded.Name = newName;
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.Equal(newName, loaded.Name);
+            }
+        }
+
+
+        [Fact]
+        public void Can_delete_Mitarbeitert()
+        {
+            var m = new Mitarbeiter { Name = $"Fred_{Guid.NewGuid()}" };
+            using (var con = new EfContext())
+            {
+                con.Mitarbeiter.Add(m);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.NotNull(loaded);
+                con.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.Null(loaded);
+            }
+        }
+
     }
 }
