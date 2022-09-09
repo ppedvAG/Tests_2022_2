d64385a2a7d85744711b01c7f140cd26aa16c37e
André <AndreR@ppedv.de>
2022-09-09 11:27:53 +0200

    Tests mit Auto Fixture

--- Stammdatenverwaltung/Stammdatenverwaltung.Data.Tests/EfContextTests.cs
+++ Stammdatenverwaltung/Stammdatenverwaltung.Data.Tests/EfContextTests.cs
@@ -1,6 +1,10 @@
+using AutoFixture;
+using AutoFixture.Kernel;
 using Stammdatenverwaltung.Data.Model;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace Stammdatenverwaltung.Data.Tests
@@ -85,6 +89,48 @@ namespace Stammdatenverwaltung.Data.Tests
             {
                 var loaded = con.Mitarbeiter.Find(m.Id);
                 Assert.Null(loaded);
+            }
+        }
+
+
+
+        [Fact]
+        public void Can_add_new_Mitarbeitert_to_db_AutoFix()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+            var m = fix.Create<Mitarbeiter>();
+
+            using (var con = new EfContext())
+            {
+                con.Mitarbeiter.Add(m);
+                con.SaveChanges();
+            }
+
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.Equal(m.Name, loaded.Name);
+            }
+        }
+
+        internal class PropertyNameOmitter : ISpecimenBuilder
+        {
+            private readonly IEnumerable<string> names;
+
+            internal PropertyNameOmitter(params string[] names)
+            {
+                this.names = names;
+            }
+
+            public object Create(object request, ISpecimenContext context)
+            {
+                var propInfo = request as PropertyInfo;
+                if (propInfo != null && names.Contains(propInfo.Name))
+                    return new OmitSpecimen();
+
+                return new NoSpecimen();
             }
         }
 
